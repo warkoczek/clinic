03b73be8af37f6c6148ccb30d5aee5209201b67e
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-03-13 16:05:28 +0100

    dto

--- src/main/java/com/example/clinic/controller/PatientController.java
+++ src/main/java/com/example/clinic/controller/PatientController.java
@@ -1,5 +1,8 @@
 package com.example.clinic.controller;
 
+import com.example.clinic.exception.PatientNotFoundException;
+import com.example.clinic.model.Address;
+import com.example.clinic.model.AddressDTO;
 import com.example.clinic.model.Patient;
 import com.example.clinic.service.PatientService;
 import org.springframework.http.HttpStatus;
@@ -36,19 +39,29 @@ public class PatientController {
         return ResponseEntity.ok(patient.get());
     }
 
-    @PostMapping(value = "/patients/add", consumes = "application/json")
+    @PostMapping(value = "/patient/add", consumes = "application/json")
     public ResponseEntity<String> submitPatient(@RequestBody Patient patient){
         return new ResponseEntity<>(patientService.addPatient(patient), HttpStatus.CREATED);
     }
 
-    @PutMapping(value = "patients/update", consumes = "application/json")
+    @PutMapping(value = "/patient/update", consumes = "application/json")
     public ResponseEntity<Patient> updatePatient(@RequestBody Patient patient){
         return new ResponseEntity<>(patientService.updatePatient(patient), HttpStatus.CREATED);
     }
 
-    @DeleteMapping(value = "/patients/delete/{username}")
+    @DeleteMapping(value = "/patient/delete/{username}")
     public ResponseEntity<Void> deletePatient(@PathVariable String username){
         patientService.removePatientByUsername(username);
         return noContent().build();
+    }
+
+    @GetMapping(value = "/patient/address", produces = "application/json")
+    public ResponseEntity<AddressDTO> getAddressByPatientId(@RequestParam(required = false) String username){
+
+        Optional<AddressDTO> addressByPatientDTO = patientService.retrieveAddressByPatientId(username);
+        if(addressByPatientDTO.isPresent()){
+            return ResponseEntity.ok(addressByPatientDTO.get());
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
--- src/main/java/com/example/clinic/model/AddressDTO.java
+++ src/main/java/com/example/clinic/model/AddressDTO.java
@@ -1,21 +1,10 @@
 package com.example.clinic.model;
 
-import lombok.Data;
-
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import java.util.Objects;
 
-@Entity
-@Data
-public class Address {
 
-    @Id
-    @GeneratedValue(generator = "addressSeq")
-    @SequenceGenerator(name = "addressSeq", sequenceName = "address_seq", allocationSize = 1)
-    private Long id;
+public class AddressDTO {
 
     private String street;
     private String houseNumber;
@@ -23,23 +12,15 @@ public class Address {
     private String city;
     private String country;
 
-    public Address() {
+    public AddressDTO() {
     }
 
-    public Address(String street, String houseNumber, String postCode, String city, String country){
-        this.street=street;
-        this.houseNumber=houseNumber;
-        this.postCode=postCode;
-        this.city=city;
-        this.country=country;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+    public AddressDTO(String street, String houseNumber, String postCode, String city, String country) {
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.postCode = postCode;
+        this.city = city;
+        this.country = country;
     }
 
     public String getStreet() {
@@ -86,17 +67,16 @@ public class Address {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Address address = (Address) o;
-        return Objects.equals(id, address.id) &&
-                Objects.equals(street, address.street) &&
-                Objects.equals(houseNumber, address.houseNumber) &&
-                Objects.equals(postCode, address.postCode) &&
-                Objects.equals(city, address.city) &&
-                Objects.equals(country, address.country);
+        AddressDTO that = (AddressDTO) o;
+        return Objects.equals(street, that.street) &&
+                Objects.equals(houseNumber, that.houseNumber) &&
+                Objects.equals(postCode, that.postCode) &&
+                Objects.equals(city, that.city) &&
+                Objects.equals(country, that.country);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, street, houseNumber, postCode, city, country);
+        return Objects.hash(street, houseNumber, postCode, city, country);
     }
 }
