f9682c302ea0e8710f9ae77ac4bdc57182e4c80e
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-04-15 10:59:32 +0200

    set MySQL DB

--- src/main/java/com/example/clinic/service/AddressService.java
+++ src/main/java/com/example/clinic/service/AddressService.java
@@ -7,16 +7,5 @@ import org.springframework.stereotype.Service;
 @Service
 public class AddressService {
 
-    private final AddressRepository addressRepository;
 
-    public AddressService(AddressRepository addressRepository) {
-        this.addressRepository = addressRepository;
-    }
-
-   /* public void addAddress(Address address){
-        addressRepository.findAllByCityAndPostCodeAndStreet(address.getCity(), address.getPostCode(), address.getStreet())
-                .stream()
-                .
-
-    }*/
 }
